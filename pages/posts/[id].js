09f5454b802911edc01a4dcedd107398b6143da9
okaken2022 <okaken.address@gmail.com>
2023-01-24 07:45:25 +0900

    deploy

---
+++ pages/posts/[id].js
@@ -0,0 +1,34 @@
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
+import Date from '../../components/date';
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {/* Keep the existing code here */}
+
+      {/* Replace {postData.date} with this */}
+      <Date dateString={postData.date} />
+
+      {/* Keep the existing code here */}
+    </Layout>
+  );
+}
